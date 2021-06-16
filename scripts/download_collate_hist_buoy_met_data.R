7c6c99e5706541d1433b6e2c6ce94ed19e4e2bf8
wwoel <wwoelmer@vt.edu>
2021-06-16 17:02:39 -0400

    script to download hist buoy met and collate into one file

---
+++ scripts/download_collate_hist_buoy_met_data.R
@@ -0,0 +1,42 @@
+# download buoy met data from 2007 - present
+
+#install.packages('LakeMetabolizer')
+library(LakeMetabolizer)
+library(tidyverse)
+
+# air temp 
+data  <-  "https://pasta.lternet.edu/package/data/eml/edi/234/4/1af6f9a538dd5b88de15811f26f9a04a" 
+dir.create(paste0(getwd(), '/data/buoy-data/met'))
+destination <- paste0(getwd(), '/data/buoy-data/met') # some location on your computer
+try(download.file(data,destfile = paste0(destination, '/hist_buoy_air_temp.csv'),method="curl"))
+
+# PAR
+data  <-  "https://pasta.lternet.edu/package/data/eml/edi/234/4/fa0aa3a53ea63fe082fa1816be5f2545" 
+destination <- paste0(getwd(), '/data/buoy-data/met') # some location on your computer
+try(download.file(data,destfile = paste0(destination, '/hist_buoy_PAR.csv'),method="curl"))
+
+
+# Wind
+data  <-  "https://pasta.lternet.edu/package/data/eml/edi/234/4/495bb93448cbf23198d53ba5476463df" 
+destination <- paste0(getwd(), '/data/buoy-data/met') # some location on your computer
+try(download.file(data,destfile = paste0(destination, '/hist_buoy_wind.csv'),method="curl"))
+
+hist_temp <- read.csv(paste0(getwd(), '/data/buoy-data/met/hist_buoy_air_temp.csv'))
+hist_par <- read.csv(paste0(getwd(), '/data/buoy-data/met/hist_buoy_PAR.csv'))
+hist_wind <- read.csv(paste0(getwd(), '/data/buoy-data/met/hist_buoy_wind.csv'))
+
+hist_data <- left_join(hist_temp, hist_par)
+hist_data <- left_join(hist_data, hist_wind)
+
+# remove any data except for when out at loon island (maybe we will want harbor data later?)
+hist_data <- hist_data[hist_data$location=='loon',]
+
+hist_data <- hist_data %>% 
+  # maybe should filter some of the flags?
+  mutate(WindSpeed = ifelse(is.na(AveWindSp_ms), WindSp_ms, AveWindSp_ms)) %>% 
+  select(datetime, PAR_umolm2s, AirTemp_degC, WindSpeed) 
+
+colnames(hist_data) <- c('time', 'par', 'AirTemp', 'WindSpeed')
+
+par.to.sw(data = hist_data, par = hist_data$par)
+  
